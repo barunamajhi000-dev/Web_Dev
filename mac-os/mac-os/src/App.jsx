4bc63994833e7d63935ca100018c7d00f4a972ad
BarunaMajhi_Dev <barunamajhi800@gmail.com>
2026-01-20 23:43:59 +0530

    day1

---
+++ mac-os/mac-os/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./app.scss";
+import Dock from "./Components/Dock";
+const App = () => {
+  return (
+    <div>
+      <main>
+        <Dock />
+      </main>
+    </div>
+  );
+};
+
+export default App;
