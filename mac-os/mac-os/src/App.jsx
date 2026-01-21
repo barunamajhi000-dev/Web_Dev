784a1668b2993e0ee3f3de9c952ee790cd553e09
BarunaMajhi_Dev <barunamajhi800@gmail.com>
2026-01-22 00:30:31 +0530

    day2 work done

--- mac-os/mac-os/src/App.jsx
+++ mac-os/mac-os/src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import "./app.scss";
 import Dock from "./Components/Dock";
+import MacWindow from "./Components/windows/MacWindow";
 const App = () => {
   return (
     <div>
-      <main>
-        <Dock />
-      </main>
+      <Dock />
+      <MacWindow>
+        <h2>hello</h2>
+      </MacWindow>
     </div>
   );
 };
